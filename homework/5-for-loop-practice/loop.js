2126f8c4aa0b6a7f9aadf4f46bc2957fcbb73984
Chris George <cgeorge@thoughtworks.com>
2014-04-03 22:51:23 -0400

    adding in for loop practice exercise

---
+++ homework/5-for-loop-practice/loop.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+	$('#problem1 button').click(function() {
+
+		var colors = ["red", "blue", "green", "yellow", "gray"];
+		var answer = "";
+		
+		// for (var i = 0; ?; i++) {
+		// 	answer = answer + " " + ?;
+		// }
+
+		$('#answer1').text(answer);
+
+	});
+
+	$('#problem2 button').click(function() {
+
+		var names = ["Edwin", "Natasha", "Manish", "Chris"];
+		var answer = "";
+
+		// Add code here
+
+		$('#answer2').text(answer);
+
+	});
+
+	$('#problem3 button').click(function() {
+
+		var numbers = [20, 40, 60, 80, 100, 120];
+		var answer = "";
+
+		// Add code here
+
+		$('#answer3').text(answer)
+
+	});
+
+	$('#problem4 button').click(function() {
+
+		var answer = "";
+
+		// Add code here
+
+		$('#answer4').text(answer)
+
+	});
+});
